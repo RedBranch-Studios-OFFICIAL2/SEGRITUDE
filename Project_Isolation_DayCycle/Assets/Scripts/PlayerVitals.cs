ffc767ddd289ce360e4318c73f677c967f2cdf32
Divith <divithphogatgp@gmail.com>
2018-06-19 20:41:56 +0530

    Added Player Vitals Scripts
    Player vitals script has been added , which monitors health,thirst and hunger. It also constantly decreases thirst and hunger , at a rate decided by you.
    P.S , try making the mining script , I'm not finding much time these days

---
+++ Project_Isolation_DayCycle/Assets/Scripts/PlayerVitals.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerVitals : MonoBehaviour
+{
+    public Slider healthSlider;
+    public int maxHealth;
+    public int healthFallRate;
+
+    public Slider thirstSlider;
+    public int maxThirst;
+    public int thirstFallRate;
+
+    public Slider hungerSlider;
+    public int maxHunger;
+    public int hungerFallRate;
+
+    void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+
+        thirstSlider.maxValue = maxThirst;
+        thirstSlider.value = maxThirst;
+
+        hungerSlider.maxValue = maxHunger;
+        hungerSlider.value = maxHunger;
+    }
+
+    void Update()
+    {
+        //HEALTH CONTROL SECTION
+        if (hungerSlider.value <= 0 && (thirstSlider.value <= 0))
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate * 2;
+        }
+
+        else if (hungerSlider.value <= 0 || thirstSlider.value <= 0)
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate;
+        }
+
+        if (healthSlider.value <= 0)
+        {
+            CharacterDeath();
+        }
+
+        //HUNGER CONTROL SECTION
+        if (hungerSlider.value >= 0)
+        {
+            hungerSlider.value -= Time.deltaTime / hungerFallRate;
+        }
+
+        else if (hungerSlider.value <= 0)
+        {
+            hungerSlider.value = 0;
+        }
+
+        else if (hungerSlider.value >= maxHunger)
+        {
+            hungerSlider.value = maxHunger;
+        }
+
+        //THIRST CONTROL SECTION
+        if (thirstSlider.value >= 0)
+        {
+            thirstSlider.value -= Time.deltaTime / thirstFallRate;
+        }
+
+        else if (thirstSlider.value <= 0)
+        {
+            thirstSlider.value = 0;
+        }
+
+        else if (thirstSlider.value >= maxThirst)
+        {
+            thirstSlider.value = maxThirst;
+        }
+    }
+
+    void CharacterDeath()
+    {
+            //DEATH
+    }
+}
