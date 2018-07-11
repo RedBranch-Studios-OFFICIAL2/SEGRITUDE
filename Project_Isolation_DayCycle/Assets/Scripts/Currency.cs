f6360799ddb559a7a7ebec00baf11e965653cca2
Bogzilla808 <40321051+Bogzilla808@users.noreply.github.com>
2018-07-11 14:10:05 +0300

    Coins script
    Script that displays the number of coins on the UI.

---
+++ Project_Isolation_DayCycle/Assets/Scripts/Currency.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour 
+{
+	[Header("Currency")]
+	public float coins;
+	[Header("UI")]
+	public Text coinsDisplay;
+
+	void Update()
+	{
+		coinsDisplay.text = "Coins: " + coins;
+
+		if (Input.GetKeyDown(KeyCode.T)) //Adds a coin when T is pressed
+		{
+			coins += 1;
+		}
+	}
+}
