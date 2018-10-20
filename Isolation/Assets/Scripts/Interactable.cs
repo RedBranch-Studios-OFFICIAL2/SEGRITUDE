736b62bef25c9b6f5c2885df2e0f311a468b2596
REDPlays <40319851+REDPlays@users.noreply.github.com>
2018-10-20 19:56:55 -0400

    Large Update #2

---
+++ Isolation/Assets/Scripts/Interactable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : InventorySystem {
+
+    public GameObject NewItem;
+
+    public bool Insert = false;
+
+    public void OnMouseDown()
+    {
+        PickUp(NewItem);
+        
+    }
+}
---
+++ Isolation/Assets/Scripts/SlotSelection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotSelection : MonoBehaviour {
+
+    public GameObject Slot;
+
+    public GameObject StoredObject;
+
+    public void OnSlotSelect()
+    {
+        StoredObject = Slot.GetComponent<SlotScript>().StoredObject;
+
+        Debug.Log(StoredObject);
+    }
+}
