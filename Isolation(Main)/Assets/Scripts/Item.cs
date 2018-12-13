4f64efb6282317665feb1e1366b8a1e4d151564f
REDPlays <40319851+REDPlays@users.noreply.github.com>
2018-12-12 19:53:35 -0500

    Random Update

--- Isolation(Main)/Assets/Scripts/Item.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName ="Item",menuName ="Create_Item")]
-public class Item : ScriptableObject {
-
-    public string nameItem;
-    //the inventar guy you can have fun with the implementation
-}
