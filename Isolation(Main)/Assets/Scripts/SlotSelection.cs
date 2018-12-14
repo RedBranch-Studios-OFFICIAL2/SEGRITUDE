8baec986a963636f56e66a7c21cbb91421a39372
Caleb Lawrence <39496692+CLaw-57@users.noreply.github.com>
2018-12-14 15:05:52 -0500

    Update

--- Isolation(Main)/Assets/Scripts/SlotSelection.cs
+++ Isolation(Main)/Assets/Scripts/SlotSelection.cs
@@ -8,18 +8,63 @@ public class SlotSelection : InventorySystem {
     public GameObject StoredObject;
     public GameObject Player;
 
+    public Interactable[] Items;
+
+    public bool Active = false;
+
     public void OnSlotSelect()
     {
-        StoredObject = gameObject.transform.parent.GetComponent<SlotScript>().StoredObject;
-        Debug.Log("Slot Selected");
+        if (gameObject.transform.parent.GetComponent<SlotScript>().StoredObject != null)
+        {
+            Items = gameObject.transform.parent.GetComponentsInChildren<Interactable>();
 
-        StoredObject.transform.parent = null;
-        StoredObject.SetActive(true);
-        StoredObject.transform.position = Player.transform.position + Player.transform.forward;
-        StoredObject = null;
+            // Removes an Item from the slot
+            StoredObject = gameObject.transform.parent.GetComponent<SlotScript>().StoredObject;
+            gameObject.transform.parent.GetComponent<SlotScript>().AmObjects = gameObject.transform.parent.GetComponent<SlotScript>().AmObjects - 1;
+            gameObject.transform.parent.GetComponent<SlotScript>().Amount.text = gameObject.transform.parent.GetComponent<SlotScript>().AmObjects.ToString();
 
-        gameObject.transform.parent.GetComponent<SlotScript>().StoredObject = null;
+             // Sets next Item in slot
+            gameObject.transform.parent.GetComponent<SlotScript>().Objects.Remove(StoredObject);
 
-        gameObject.transform.parent.GetComponent<Image>().color = new Color32(0, 0, 0, 150);
+            //gameObject.transform.parent.GetComponent<SlotScript>().StoredObject = gameObject.transform.parent.GetComponent<SlotScript>().Objects. ;
+            StoredObject = gameObject.transform.parent.GetComponent<SlotScript>().StoredObject;
+
+            Debug.Log("Slot Selected");
+            // Places the Removed Item in front of the character
+            StoredObject.transform.parent = null;
+            StoredObject.SetActive(true);
+            StoredObject.transform.position = Player.transform.position + Player.transform.forward;
+            StoredObject = null;
+
+            gameObject.transform.parent.GetComponent<SlotScript>().StoredObject = null;
+
+            if (gameObject.transform.parent.GetComponent<SlotScript>().AmObjects > 0)
+            {
+                return;
+            }
+            else if (gameObject.transform.parent.GetComponent<SlotScript>().AmObjects == 0)
+            {
+                gameObject.transform.parent.GetComponent<Image>().color = new Color32(0, 0, 0, 150);
+            }
+        }
+        else if (gameObject.transform.parent.GetComponent<SlotScript>().StoredObject == null)
+        {
+            Debug.Log("Nothing Found!");
+            //Active = true;
+            return;
+        }
     }
+
+    /*public void Update()
+    {
+        if(Active == true)
+        {
+            Debug.Log("Slot Changing!");
+            gameObject.transform.position = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Active = false;
+        }
+    }*/
 }
